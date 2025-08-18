9654f3cb00d7aba8b1fdb10d52e503d0fdad2df0
Moteen Raza <moteenraza@gmail.com>
2025-08-18 19:15:36 -0400

    step 2 complete with sub classes used milk and yogurt as an example

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ class ProductProperties {
 }
 
 const orange = new ProductProperties("Orange", 4, 3);
-
+// example
 console.log(orange.toString());
 console.log("The total value is: $", orange.getTotalValue());
 
@@ -31,4 +31,19 @@ class PerishableProductProperties extends ProductProperties {
     super(name, price, quantity);
     this.expirationDate = expirationDate;
   }
+  //Override toString method
+  toString() {
+    return `Product: ${this.name}, Price: $${this.price}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}`;
+  }
 }
+
+//example for milk and yogurt
+const milk = new PerishableProductProperties("milk", 1.5, 10, "2025-08-22");
+const yogurt = new PerishableProductProperties("Yogurt", 4.5, 2, "2025-08-30");
+
+//console log milk output
+console.log(milk.toString());
+console.log("The total value is: $" + milk.getTotalValue());
+
+console.log(yogurt.toString());
+console.log("The total value is: $" + yogurt.getTotalValue());
